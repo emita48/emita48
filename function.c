d796efdbd4a6c000eaf8ca5f4989e1f062d052fc
JDC <emita48@gmail.com>
2020-11-19 12:45:53 +0900

    function.c

--- function.c
+++ function.c
@@ -1,7 +1,12 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<stdlib.h>
-void p(int num, int num2, int answer);
-int grade;
+#include<time.h>
+//void p(int num, int num2, int answer);
+//int grade;
+int getRandomNumber(int level);
+void showQuestion(int level, int num1, int num2);
+void success();
+void fail();
 int main(void) /* {
 	int num = 2;
 	num += 3;
@@ -25,7 +30,7 @@ int main(void) /* {
 			num += 3;
 		}
 	}
-}*/
+}
 
 {
 	while (1) {
@@ -59,4 +64,49 @@ void p(int num, int num2, int answer){
 		}
 		return 1;
 	}
+}*/
+{
+	srand(time(NULL));
+	int count = 0;
+	for (int i = 1; i <= 5; i++) {
+		int num1 = getRandomNumber(i);
+		int num2 = getRandomNumber(i);
+		showQuestion(i, num1, num2);
+		int answer = -1;
+		scanf_s("%d", &answer);
+		if (answer == -1) {
+			printf("프로그램을 종료합니다");
+			exit(0);
+		}
+		else if (answer == num1 * num2) {
+			success();
+			count++;
+		}
+		else {
+			fail();
+		}
+	}
+	printf("당신은 %d개 만큼 정답을 맞췄습니다.\n", count);
+	return 0;
+}
+
+int getRandomNumber(int level)
+{
+	return rand() & (level * 7) + 1;
+}
+
+void showQuestion(int level, int num1, int num2) 
+{
+	printf("\n\n\n######### %d 번쨰 비밀번호 #########\n", level);
+	printf("\n\t%d x %d 는? \n\n", num1, num2);
+	printf("##################################\n");
+	printf("\n비밀번호를 입력하세요 (종료 : -1) >>");
+}
+
+void success() {
+	printf("정답입니다.\n");
+}
+
+void fail() {
+	printf("틀렸습니다.\n");
 }
